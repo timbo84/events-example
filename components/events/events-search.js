7d81fdf4295301e94225e913b16da6075eecaeef
timbo84 <timber110406@gmail.com>
2023-05-17 15:20:05 -0700

    updated dates

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -22,8 +22,10 @@ function EventsSearch(props) {
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
           <select id="year" ref={yearInputRef}>
-            <option value="2021">2021</option>
+            <option value="2020">2023</option>
+            <option value="2021">2023</option>
             <option value="2022">2022</option>
+            <option value="2023">2023</option>
           </select>
         </div>
         <div className={classes.control}>
